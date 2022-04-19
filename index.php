6955eec2a4552fa86b808e256bcc970cfd10b4f7
dean-mcbean <dean.w.mcbean@gmail.com>
2022-04-19 14:37:36 +1200

    u

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
         <!-- JQUERY -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
+        <!-- FIREBASE & FIRESTORE -->
+        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+
         <!-- AUTH UI -->
         <script src="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js"></script>
         <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.css" />
